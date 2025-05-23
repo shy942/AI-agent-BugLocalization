3abf594f1986c8eda432e72198aaac7ed01ad8da
baolinyang1 <baoliny961@gmail.com>
2025-05-23 14:15:48 -0600

    USELESS comments

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export PYTHONPATH=src
 mkdir -p logs/terminal_logs
-python src/KEYBERT.py > logs/terminal_logs/keybert_log.txt 2>&1 &   # Run in background
+python src/KEYBERT.py > logs/terminal_logs/keybert_log.txt 2>&1 &   
 python src/NLP.py > logs/terminal_logs/NLP_log.txt 2>&1 &  
-python src/reason.py > logs/terminal_logs/reason_log.txt 2>&1 &   # Also run in background
+python src/reason.py > logs/terminal_logs/reason_log.txt 2>&1 &   
 
 wait  # Wait for both scripts to finish
 
